415826cd422b11d2dc4f57401ce3a30779526213
Michelle Betancurt <michelbarros178@gmail.com>
2026-03-12 16:50:53 -0500

    cambios

---
+++ Maquetacion Pagina web Kuky Pets/view/nuevo_login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Design by foolishdeveloper.com -->
+    <title>Iniciar sesión – Kuky Pets</title>
+ 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+   <link rel="icon" href="../images/favicon.ico" type="image/x-icon" />
+    <!--Stylesheet-->
+    <link rel="stylesheet" href="../styles/lg.css">
+</head>
+<body >
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form class="formulario" id="formulario" action="inicio.php" method="POST">
+        <h3>INCIO DE SESIÓN</h3>
+
+        <div class="inputBx">
+        <label for="username">Usuario</label>
+        <input type="text" id="username"  placeholder="Ej: Juan Pérez" name="nombre_completo" required>
+        </div>
+        <div class="inputBx">
+        <label for="password">Contraseña</label>
+        <input type="password" placeholder="Ej: 123456" name="password" required>
+        </div>
+        <div class="inputBx">
+        <button type="submit">Iniciar Sesión</button>
+        </div>
+        <div class="social">
+        <a href="registrate.php"></a><div class="fb"></i>Registrarse</div>
+          <a href="index.html"></a><div class="fb"></i>volver</div>
+        </div>
+    </form>
+</body>
+</html>
--- Maquetacion Pagina web Kuky Pets/view/registrate.php
+++ Maquetacion Pagina web Kuky Pets/view/registrate.php
@@ -1,44 +1,50 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Iniciar sesión – Kuky Pets</title>
-   <link rel="icon" href="../images/favicon.ico" type="image/x-icon" />
-  <link rel="stylesheet" href="../styles/login.css" />
-  <!-- Íconos Font Awesome -->
+    <title>Registrate – Kuky Pets</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+    <link rel="icon" href="../images/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="../styles/lg.css">
 </head>
 <body>
+    <div class="background">
+        <div class="shape"></div>
+        <div class="shape"></div>
+    </div>
+    <form class="formulario" id="formulario" action="registrarse.php" method="POST">
+        <h3>REGÍSTRATE</h3>
 
-<!--ring div starts here-->
-<form class="formulario" id="formulario" action="registrarse.php" method="POST">
-<div class="ring">
-  <i style="--clr:#ff9900;"></i>
-  <i style="--clr:#00fffb;"></i>
-  </i>
-  <div class="login">
-    <h2>Registrate</h2><br>
+        <div class="inputBx" id="grupo__username">
+            <label for="username">Usuario</label>
+            <input type="text" id="username" placeholder="Usuario" name="username" required>
+            <i class="formulario__validacion-estado fas fa-times-circle"></i>
+        </div>
 
-    <div class="inputBx">
-      <input type="text" placeholder="Usuario" name="username" required>
-    </div>
-    <div class="inputBx">
-      <input type="text" placeholder="Contraseña" name="password_hash" required>
-    </div>
-    <div class="inputBx">
-      <input type="text" placeholder="Correo" name="email" required>
-    </div>
-    <div class="inputBx">
-    <input type="submit" value="Registrarse">
-    </div>
-        <div class="links">
-      <a href="login.php" class="btn-gradiente-volver">Volver</a>
-    </div>
-  </div>
-</div>
+        <div class="inputBx" id="grupo__password">
+            <label for="password">Contraseña</label>
+            <input type="password" id="password" placeholder="Mínimo 4 caracteres" name="password" required>
+            <i class="formulario__validacion-estado fas fa-times-circle"></i>
+        </div>
 
-</form>
-<!--ring div ends here-->
-<script src="velidaciones.js"></script>
+        <div class="inputBx" id="grupo__email">
+            <label for="email">Correo Electrónico</label>
+            <input type="email" id="email" placeholder="ejemplo@correo.com" name="email" required>
+            <i class="formulario__validacion-estado fas fa-times-circle"></i>
+        </div>
+
+        <div class="inputBx">
+            <button type="submit">Crear Cuenta</button>
+        </div>
+
+        <div class="social">
+            <a href="login.php" style="text-decoration: none;">
+                <div class="fb">Volver al Login</div>
+            </a>
+        </div>
+    </form>
+
+    <script src="../js/registrarse_validaciones.js"></script>
 </body>
 </html>
